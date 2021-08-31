aafb3311f26155fb6c8f0d8f6eb1327ecfcd099a
Evan <ehutch7@gmail.com>
2021-08-31 11:05:18 -0400

    Adding create task method and task model.

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Task } from "./task.model";
 import { TasksService } from "./tasks.service";
 
 @Controller("tasks")
 export class TasksController {
-  constructor(private tasksService: TasksService) {}
+    constructor(private tasksService: TasksService) {}
+
+    @Post()
+    public createTask(@Body() body: Partial<Task>): Task {
+        return this.tasksService.createTask(body.title, body.description);
+    }
+
+    @Get()
+    public getAllTasks(): Task[] {
+        return this.tasksService.getAllTasks();
+    }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { Task, TaskStatus } from "./task.model";
+import { uuid4 } from "uuid";
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    private tasks: Task[] = [];
+
+    public createTask(title: string, description: string): Task {
+        const task: Task = {
+            title,
+            description,
+            id: uuid4(),
+            status: TaskStatus.OPEN,
+        };
+
+        this.tasks.push(task);
+        return task;
+    }
+
+    public getAllTasks(): Task[] {
+        return this.tasks;
+    }
+}
